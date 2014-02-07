cf8810592db01b750bcfbb6af3312f04a5abbc1b
Wouter van Drunen <wvandrunen@gmail.com>
2014-02-07 16:08:03 +0100

    First try at homework 5.1

---
+++ hw5.1/author_with_most_comments.js
@@ -0,0 +1,22 @@
+use blog
+db.posts.aggregate([
+	{
+		$unwind : "$comments"
+	}, 
+	{
+		$group : 
+		{ 
+			"_id" : "$comments.author",
+			"posts" : { "$sum" : 1 } 
+		}
+	}, 
+	{
+		$sort :
+		{
+			posts : 1
+		}
+	},
+	{
+		$limit : 10
+	}
+])
